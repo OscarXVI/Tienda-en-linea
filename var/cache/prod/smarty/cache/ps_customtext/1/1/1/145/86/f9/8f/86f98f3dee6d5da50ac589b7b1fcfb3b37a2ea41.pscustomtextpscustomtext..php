0694063e28142ad9469eee1fb5d23a43728f862c
Maria Ibarra <48442545+maj0ibarra@users.noreply.github.com>
2020-05-02 00:59:40 -0500

    prueba

---
+++ var/cache/prod/smarty/cache/ps_customtext/1/1/1/145/86/f9/8f/86f98f3dee6d5da50ac589b7b1fcfb3b37a2ea41.pscustomtextpscustomtext..php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-05-02 00:54:15
+  from 'module:pscustomtextpscustomtext.' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ead0b0725ef79_40398108',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8654b2ba7ef103395c5eb0a4a12ed7463d291bc8' => 
+    array (
+      0 => 'module:pscustomtextpscustomtext.',
+      1 => 1532078710,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_5ead0b0725ef79_40398108 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="custom-text">
+  <h1><strong>Un Dulce Bienestar Para Toda Tu Familia y si xd 1</strong></h1>
+</div>
+<?php }
+}

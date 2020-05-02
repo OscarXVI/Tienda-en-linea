af93a375c11373bb4e8b777e177c2d3f09efbade
OscarXVI <48355448+OscarXVI@users.noreply.github.com>
2020-05-02 02:01:51 -0500

    Revert "Update 86f98f3dee6d5da50ac589b7b1fcfb3b37a2ea41.pscustomtextpscustomtext..php"
    This reverts commit 187cd131fb3bdbfa913fbbc79a5dfcdfac553545.

--- var/cache/prod/smarty/cache/ps_customtext/1/1/1/145/86/f9/8f/86f98f3dee6d5da50ac589b7b1fcfb3b37a2ea41.pscustomtextpscustomtext..php
+++ var/cache/prod/smarty/cache/ps_customtext/1/1/1/145/86/f9/8f/86f98f3dee6d5da50ac589b7b1fcfb3b37a2ea41.pscustomtextpscustomtext..php
@@ -1,19 +1,11 @@
 <?php
-<<<<<<< Updated upstream
 /* Smarty version 3.1.33, created on 2020-05-02 01:31:04
-=======
-/* Smarty version 3.1.33, created on 2020-05-02 01:59:50
->>>>>>> Stashed changes
   from 'module:pscustomtextpscustomtext.' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-<<<<<<< Updated upstream
   'unifunc' => 'content_5ead13a85053e9_38008972',
-=======
-  'unifunc' => 'content_5ead1a66ab39f9_67487989',
->>>>>>> Stashed changes
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -26,17 +18,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-<<<<<<< Updated upstream
 function content_5ead13a85053e9_38008972 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div id="custom-text">
   <h1><strong>Un Dulce Bienestar Para Toda Tu Familia</strong></h1>
-=======
-function content_5ead1a66ab39f9_67487989 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-<div id="custom-text">
-  <h1><strong>Un Dulce Bienestar Para Toda Tu Familia. .</strong></h1>
->>>>>>> Stashed changes
 </div>
 <?php }
 }

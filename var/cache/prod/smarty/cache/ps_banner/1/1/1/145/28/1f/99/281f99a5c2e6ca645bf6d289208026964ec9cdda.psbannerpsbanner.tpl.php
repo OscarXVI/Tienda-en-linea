625829ca7891f904efc153ba0801703b348ad96a
Edgar-lazaro <edgarlazaro25@gmail.com>
2020-05-02 01:35:25 -0500

    prueba2

---
+++ var/cache/prod/smarty/cache/ps_banner/1/1/1/145/28/1f/99/281f99a5c2e6ca645bf6d289208026964ec9cdda.psbannerpsbanner.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-05-02 01:34:58
+  from 'module:psbannerpsbanner.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ead1492f17727_34031844',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '64034093fbda864710a2c0f526b37f921e53ee39' => 
+    array (
+      0 => 'module:psbannerpsbanner.tpl',
+      1 => 1587040546,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_5ead1492f17727_34031844 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="https://support-leagueoflegends.riotgames.com/hc/en-us/requests/new?ticket_form_id=652587" title="https://support-leagueoflegends.riotgames.com/hc/en-us/requests/new?ticket_form_id=652587">
+      <img src="http://localhost/tienda/modules/ps_banner/img/e7e64406cb0e366388a8f6a2450ce7bf.jpg" alt="https://support-leagueoflegends.riotgames.com/hc/en-us/requests/new?ticket_form_id=652587" title="https://support-leagueoflegends.riotgames.com/hc/en-us/requests/new?ticket_form_id=652587" class="img-fluid">
+  </a>
+<?php }
+}
